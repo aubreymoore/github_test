3c43040ea93bd101fe4a51cc779d5d8cb28c340d
aubreymoore <aubreymoore@guam.net>
2025-03-12 10:00:03 +1000

    added by gitpush.sh

--- test.py
+++ test.py
@@ -34,7 +34,7 @@ df['timestamp'] = pd.to_datetime(df['timestamp'])
 df['HM'] = df['timestamp'].dt.strftime('%H:%M')
 df['download_Mbps'] = df['download_Mbps'].astype(float)
 df['upload_Mbps'] = df['upload_Mbps'].astype(float)
-ic(df)
+# ic(df)
 
 # Plot the data and save as PNG
 df.plot(
