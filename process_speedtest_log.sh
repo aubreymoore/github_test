9f7d80f32a25fc8c59fe4d5c044a991e0a882a73
aubreymoore <aubreymoore@guam.net>
2025-03-20 07:00:02 +0800

    added by process_speedtest_log.sh

--- process_speedtest_log.sh
+++ process_speedtest_log.sh
@@ -1,7 +1,14 @@
-cd /home/aubrey/Desktop/git_test
+# Change to git_test directory
+cd /home/aubrey/Desktop/git_test 
+
+# Activate virtual environment
 source .venv/bin/activate
+
+# Process log file
 python3 test.py
+
+# Synchronize with GitHub repository
 git pull
 git add .
-git commit -m 'added by gitpush.sh'
+git commit -m 'added by process_speedtest_log.sh'
 git push
