4843938c85c4b934e4e8a6ba87a86a7539a7f75c
aubreymoore <aubreymoore@guam.net>
2025-03-20 06:48:09 +0800

    added by gitpush.sh

--- process_speedtest_log.sh
+++ process_speedtest_log.sh
@@ -1,7 +1,6 @@
 cd /home/aubrey/Desktop/git_test
 source .venv/bin/activate
-# /home/aubrey/Desktop/git_test/.venv/bin/python /home/aubrey/Desktop/git_test/test.py
-python test.py
+python3 test.py
 git pull
 git add .
 git commit -m 'added by gitpush.sh'
